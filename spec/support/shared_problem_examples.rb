33523ac2fc0095221a373f69a4215c8bebd19e3f
ChaYoung You <yousbe@gmail.com>
2015-01-22 06:01:02 +0900

    Mock `ARGF.map` and share the input for test

--- spec/support/shared_problem_examples.rb
+++ spec/support/shared_problem_examples.rb
@@ -1,6 +1,10 @@
 RSpec.shared_examples 'a problem' do
   it 'runs' do
-    allow_any_instance_of(Kernel).to receive(:gets).and_return(*input.lines)
+    mocked_input = StringIO.new(input)
+    allow_any_instance_of(Kernel).to receive(:gets) { mocked_input.gets }
+    allow(ARGF).to receive(:map) do |*args, &block|
+      mocked_input.map(*args, &block)
+    end
     require subject
     expect($stdout.string).to eq(output)
   end
