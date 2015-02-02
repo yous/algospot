c965a2067690ae9f22fd0af95aba7fd1b74703d3
ChaYoung You <yousbe@gmail.com>
2015-02-02 18:59:55 +0900

    Mock `ARGF.each_slice`

--- spec/support/shared_problem_examples.rb
+++ spec/support/shared_problem_examples.rb
@@ -5,6 +5,9 @@ RSpec.shared_examples 'a problem' do
     allow(ARGF).to receive(:map) do |*args, &block|
       mocked_input.map(*args, &block)
     end
+    allow(ARGF).to receive(:each_slice) do |*args, &block|
+      mocked_input.each_slice(*args, &block)
+    end
     require subject
     expect($stdout.string).to eq(output)
   end
